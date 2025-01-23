6c823d95e91cf956af2878eaadd30c790a6d0123
Mohamed Mostafa <mohamedmostafa3730@gmail.com>
2025-01-23 22:28:18 +0200

    twoSum => HashMap with two ways (one pass,two pass)

--- src/twoSum.java
+++ src/twoSum.java
@@ -1,5 +1,7 @@
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class twoSum {
 
@@ -12,8 +14,7 @@ public class twoSum {
     /* 
      * time complicity => O(n^2)
      * memory complicity => O(1)
-    */
-
+     */
     public static int[] twoSumBruteForce(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -23,6 +24,41 @@ public class twoSum {
             }
         }
         return new int[0];
+    }
+
+    /**
+     * time complicity => O(n) 
+     * memory complicity => O(n)
+     */
+    public static int[] towSumHashMaPTwoPass(int[] nums, int target) {
+        Map<Integer, Integer> twoSumMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            twoSumMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (twoSumMap.containsKey(diff) && twoSumMap.get(diff) != i) {
+                return new int[]{i, twoSumMap.get(diff)};
+            }
+        }
+
+        return new int[0];
+    }
+
+    public static int[] towSumHashMaPOnePass(int[] nums, int target) {
+        HashMap<Integer, Integer> twoSumMap = new HashMap<>();
+        int num, diff;
+        for (int i = 0; i < nums.length; i++) {
+            num = nums[i];
+            diff = target - num;
+            if (twoSumMap.containsKey(diff)) {
+                return new int[]{twoSumMap.get(diff), i};
+            }
+            twoSumMap.put(num, i);
+        }
+        return new int[]{0};
     }
 
 }
